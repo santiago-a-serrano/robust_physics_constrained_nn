59af9430e296ad6f070699bf479d0769098e49e9
Santiago Andrés Serrano Vacca <santiagoserrano334@gmail.com>
2024-03-01 11:56:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ def _parse_requirements(requirements_txt_path):
 setup(
     name='physics_constrained_nn',
     version='0.0.1',
-    description='A module for training neural networks of unknown dynamical systems that can incorporate physics knowledge in form of structural knowledge of constraints on the internal state of the model',
+    description='A module for training neural networks of unknown dynamical systems that can incorporate physics knowledge in form of structural knowledge of constraints on the internal state of the model. Also contains robustness improvements made by Santiago Andrés Serrano-Vacca.',
     license="GNU 3.0",
     long_description=long_description,
-    author='Franck Djeumou and Cyrus Neary',
-    author_email='fdjeumou@utexas.edu, cneary@utexas.edu',
-    url="https://github.com/wuwushrek/physics_constrained_nn.git",
+    author='Franck Djeumou, Cyrus Neary, Santiago Serrano',
+    author_email='fdjeumou@utexas.edu, cneary@utexas.edu, santiagoserrano334@gmail.com',
+    url="https://github.com/santiago-a-serrano/robust_physics_constrained_nn.git",
     packages=['physics_constrained_nn'],
     package_dir={'physics_constrained_nn': 'physics_constrained_nn'},
 )
